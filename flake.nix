9bd2fe46aaf70bec2cfc969f889183f8480f82db
dylanm-dot <figment_lawless.2a@icloud.com>
2026-09-01 15:31:08 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
 	    url = "github:nix-community/home-manager/release-26.05";
 	    inputs.nixpkgs.follows = "nixpkgs";
 	};
-	mangowm = {
-	    url = "github:mangowm/mango";
-	    inputs.nixpkgs.follows = "nixpkgs";
-	};
     };
 
     outputs = {self, nixpkgs, home-manager, ...}: {
