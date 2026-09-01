17cbe30178bed85f95e05d46ffdbff0c2603c896
dylanm-dot <figment_lawless.2a@icloud.com>
2026-09-01 18:03:41 -0400

    Delete config directory

--- config/mango/scripts/cliphist.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cliphist list | wofi --dmenu --pre-display-cmd "echo '%s' | cut -f 2" | cliphist decode | wl-copy
--- config/mango/scripts/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-!/bin/bash
-pkill waybar
-waybar &
